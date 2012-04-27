3f7a1b5227d96a11e9f5b8614680578019b0220c
Peter Drake <pdrake@allplayers.com>
2012-04-27 14:26:39 -0400

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: hyperic
+#
+# This module manages hyperic
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class hyperic {
+
+
+}
